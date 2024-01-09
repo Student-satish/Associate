03996a861ae846e94d71986da783a28ba3ed1e21
Student-satish <ksatish@23819@gmail.com>
2024-01-09 18:49:13 +0530

    increased

--- Classroom.java
+++ Classroom.java
@@ -1,6 +1,6 @@
 public class Classroom{
     public static void main(String args[]){
         System.out.println("hi satish");
-        for(int i = 0;i<8;i++) System.out.print(i+" ");
+        for(int i = 0;i<9;i++) System.out.print(i+" ");
     }
 }
