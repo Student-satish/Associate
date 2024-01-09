f9f0336395a0524b7467eb05750d186dd2e09934
Student-satish <ksatish@23819@gmail.com>
2024-01-09 19:26:06 +0530

    add a new file

--- Classroom.java
+++ Classroom.java
@@ -2,7 +2,6 @@ public class Classroom{
     public static void main(String args[]){
         System.out.println("hi satish");
         for(int i = 0;i<6;i++) System.out.print(i+" ");
-        for(int i = 0;i<9;i++) System.out.print(i+" ");
 
     }
 }
